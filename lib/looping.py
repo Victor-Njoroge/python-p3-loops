0457ee221d125894e3ef9508163b03f22bc06727
Victor Njoroge <victornjoroge2020@gmail.com>
2023-08-23 20:58:00 +0300

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 0:
+        if count > 0:
+            print(count)
+        else :
+            print("Happy New Year!")
+        
+        count -=1
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    numberArray=[]
+    for num in int_list:
+        number= num * num
+        numberArray.append(number)
+    
+    print(numberArray)
+square_integers([1, 2, 3, 4, 5])
 def fizzbuzz():
     # code goes here!
-    pass
+    i =1
+    while i <= 100:
+        if (i%3 ==0 and i % 5 !=0):
+            print("Fizz")
+        elif (i%5 ==0 and i %3 !=0):
+            print("Buzz")
+        elif (i%5 ==0 and i %3 ==0):
+            print("FizzBuzz")
+        else :
+            print(i)
+        i += 1
+fizzbuzz()
